5a24343bcc174d548e0b42b46319e8cd620d74ce
Maria <mmatrozzo@gmail.com>
2021-11-02 21:38:25 -0400

    commit tree 762a2f92cf734feffab74f89e3b614fff4567665

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`;
+    }
+
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective (flair = "*"){
+    return function innerFunction (str = "special"){
+        return `You are ${flair}${str}${flair}!`
+    } 
+    
+}
